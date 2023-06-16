5402a1c22c9fc5dce3d48e2aba3a1c6f048d4585
vinodhsukumarvictor <gpgvictor@gmail.com>
2023-06-16 12:05:41 +0100

    Revert "change to test revert"
    This reverts commit f29897f7c4b6b0044180fbd4ec4b996b0d0dc6f7.

--- lbg.py
+++ lbg.py
@@ -5,8 +5,6 @@ NB. mixes LBYL and EAFP to reflect simpler logic
 This version using simple prints to STDOUT rather than formalised logger
 """
 
-# THIS IS A CHANGE TO THE FILE
-
 # import argparse for passing PORT variable through the cli
 import argparse
 
